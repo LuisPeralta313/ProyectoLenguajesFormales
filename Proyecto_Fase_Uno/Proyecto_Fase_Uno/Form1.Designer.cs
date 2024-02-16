2c4a313bbd4c0e993660542e45e4ccb923b92f48
villelaiojo <78031180+villelaiojo@users.noreply.github.com>
2024-02-16 14:49:50 -0600

    Realizacion del Form 1 Donde se lee el archivo y se verifica la grramatica

--- Proyecto_Fase_Uno/Proyecto_Fase_Uno/Form1.Designer.cs
+++ Proyecto_Fase_Uno/Proyecto_Fase_Uno/Form1.Designer.cs
@@ -28,13 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BCargar = new System.Windows.Forms.Button();
+            this.RTBMostrarGramatica = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TBMostrarResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BCargar
+            // 
+            this.BCargar.Location = new System.Drawing.Point(115, 103);
+            this.BCargar.Margin = new System.Windows.Forms.Padding(2);
+            this.BCargar.Name = "BCargar";
+            this.BCargar.Size = new System.Drawing.Size(81, 27);
+            this.BCargar.TabIndex = 1;
+            this.BCargar.Text = "Cargar archivo";
+            this.BCargar.UseVisualStyleBackColor = true;
+            this.BCargar.Click += new System.EventHandler(this.BCargar_Click);
+            // 
+            // RTBMostrarGramatica
+            // 
+            this.RTBMostrarGramatica.Location = new System.Drawing.Point(361, 52);
+            this.RTBMostrarGramatica.Margin = new System.Windows.Forms.Padding(2);
+            this.RTBMostrarGramatica.Name = "RTBMostrarGramatica";
+            this.RTBMostrarGramatica.Size = new System.Drawing.Size(206, 190);
+            this.RTBMostrarGramatica.TabIndex = 4;
+            this.RTBMostrarGramatica.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 178);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Resultado de la gramática";
+            // 
+            // TBMostrarResultado
+            // 
+            this.TBMostrarResultado.Location = new System.Drawing.Point(97, 202);
+            this.TBMostrarResultado.Margin = new System.Windows.Forms.Padding(2);
+            this.TBMostrarResultado.Name = "TBMostrarResultado";
+            this.TBMostrarResultado.Size = new System.Drawing.Size(122, 20);
+            this.TBMostrarResultado.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(635, 310);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TBMostrarResultado);
+            this.Controls.Add(this.RTBMostrarGramatica);
+            this.Controls.Add(this.BCargar);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BCargar;
+        private System.Windows.Forms.RichTextBox RTBMostrarGramatica;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TBMostrarResultado;
     }
 }
 
--- Proyecto_Fase_Uno/Proyecto_Fase_Uno/Form1.cs
+++ Proyecto_Fase_Uno/Proyecto_Fase_Uno/Form1.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace Proyecto_Fase_Uno
 {
@@ -15,6 +18,57 @@ namespace Proyecto_Fase_Uno
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void BCargar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog AbrirArchivo = new OpenFileDialog();
+            DialogResult Resultado = AbrirArchivo.ShowDialog();
+            if (Resultado == DialogResult.OK)
+            {
+                VerificarGramatica(AbrirArchivo.FileName);
+
+            }
+            else
+            {
+                MessageBox.Show("Archivo no correcto");
+            }
+        }
+        private void VerificarGramatica(string Archivo)
+        {
+            RTBMostrarGramatica.Select(0, RTBMostrarGramatica.Lines.Length);
+
+            try
+            {
+                int linea = 0;
+                string texto = File.ReadAllText(Archivo);
+                ReglasExpresionRegular VerificarReglas = new ReglasExpresionRegular();
+                TBMostrarResultado.Text = VerificarReglas.Archivo(texto, ref linea);
+                RTBMostrarGramatica.Text = texto;
+                if (TBMostrarResultado.Text.Contains("Correcto"))
+                {
+                    TBMostrarResultado.ForeColor = Color.Green;
+                }
+                else
+                {
+                    TBMostrarResultado.ForeColor = Color.Red;
+
+                    int ContadorLinea = 0;
+                    foreach (string item in RTBMostrarGramatica.Lines)
+                    {
+                        if (linea - 1 == ContadorLinea)
+                        {
+                            RTBMostrarGramatica.Select(RTBMostrarGramatica.GetFirstCharIndexFromLine(ContadorLinea), item.Length);
+                        }
+                    }
+                    ContadorLinea++;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
