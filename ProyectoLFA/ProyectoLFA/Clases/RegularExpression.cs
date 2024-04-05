c1ce78719dcd1ad939ef0b4fd163f699314983a1
LuisPeralta313 <elperaltamc@gmail.com>
2024-04-05 00:43:12 -0600

    Agregando Lógica de Nodo, Arreglando Gramática y expresión regular que daba error

---
+++ ProyectoLFA/ProyectoLFA/Clases/RegularExpression.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoLFA.Classes
+{
+    //Gets, save and build a simple regular expression for AFD [3rd fase]
+    class RegularExpression : CharSET
+    {
+        //Constructor
+        public RegularExpression(string exp)
+        {
+            exp = simplifyExpression(exp);
+        }
+
+        private string simplifyExpression(string expression)
+        {
+            return expression;
+        }
+
+
+    }
+}
