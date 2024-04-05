e3d0e77a3a599cb3dcfbcde3611ae74fa1f13051
villelaiojo <78031180+villelaiojo@users.noreply.github.com>
2024-04-05 15:30:44 -0600

    Definicion de posibles Caracteres

---
+++ ProyectoLFA/ProyectoLFA/Clases/CharSET.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoLFA.Clases
+{
+    public class CharSET
+    {
+        public const string QuestionMark = "?";
+        public const string Grouping_Open = "(";
+        public const string Grouping_Close = ")";
+        public const string EndCharacter = "#";
+        public const string Epsilon = "ε";
+        public const string Concatenation = "●";
+        public const string Alternation = "|";
+        public const string Escape = "\\";
+        public const string Star = "*";
+        public const string Plus = "+";
+
+
+        /////////////////////////////////////////////////////////
+        
+
+
+    }
+}
---
+++ ProyectoLFA/ProyectoLFA/Clases/ET.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoLFA.Clases
+{
+    public class ET
+    {
+    }
+}
