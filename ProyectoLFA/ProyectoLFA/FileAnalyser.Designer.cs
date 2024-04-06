9c1638c58ace7929fe0a56dd93c36eb4abfe5da2
villelaiojo <78031180+villelaiojo@users.noreply.github.com>
2024-04-05 21:47:51 -0600

    Modificacion en Forms

--- ProyectoLFA/ProyectoLFA/FileAnalyser.Designer.cs
+++ ProyectoLFA/ProyectoLFA/FileAnalyser.Designer.cs
@@ -37,14 +37,15 @@ namespace ProyectoLFA
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.TransitionBTN = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // BTNUpload
             // 
-            this.BTNUpload.BackColor = System.Drawing.SystemColors.ControlText;
+            this.BTNUpload.BackColor = System.Drawing.Color.DarkOrange;
             this.BTNUpload.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BTNUpload.ForeColor = System.Drawing.SystemColors.Control;
-            this.BTNUpload.Location = new System.Drawing.Point(10, 22);
+            this.BTNUpload.Location = new System.Drawing.Point(70, 97);
             this.BTNUpload.Name = "BTNUpload";
             this.BTNUpload.Size = new System.Drawing.Size(118, 23);
             this.BTNUpload.TabIndex = 0;
@@ -54,18 +55,18 @@ namespace ProyectoLFA
             // 
             // RTBGrammar
             // 
-            this.RTBGrammar.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.RTBGrammar.Location = new System.Drawing.Point(12, 84);
+            this.RTBGrammar.BackColor = System.Drawing.SystemColors.Info;
+            this.RTBGrammar.Location = new System.Drawing.Point(298, 26);
             this.RTBGrammar.Name = "RTBGrammar";
-            this.RTBGrammar.Size = new System.Drawing.Size(370, 241);
+            this.RTBGrammar.Size = new System.Drawing.Size(369, 167);
             this.RTBGrammar.TabIndex = 1;
             this.RTBGrammar.Text = "";
             this.RTBGrammar.TextChanged += new System.EventHandler(this.RTBGrammar_TextChanged);
             // 
             // TResult
             // 
-            this.TResult.Location = new System.Drawing.Point(136, 58);
-            this.TResult.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TResult.Location = new System.Drawing.Point(18, 171);
+            this.TResult.Margin = new System.Windows.Forms.Padding(2);
             this.TResult.Name = "TResult";
             this.TResult.Size = new System.Drawing.Size(246, 20);
             this.TResult.TabIndex = 2;
@@ -75,7 +76,7 @@ namespace ProyectoLFA
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(65, 61);
+            this.label1.Location = new System.Drawing.Point(103, 152);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(67, 15);
@@ -85,18 +86,18 @@ namespace ProyectoLFA
             // 
             // TXTPath
             // 
-            this.TXTPath.Location = new System.Drawing.Point(136, 27);
-            this.TXTPath.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TXTPath.Location = new System.Drawing.Point(11, 65);
+            this.TXTPath.Margin = new System.Windows.Forms.Padding(2);
             this.TXTPath.Name = "TXTPath";
             this.TXTPath.Size = new System.Drawing.Size(246, 20);
             this.TXTPath.TabIndex = 4;
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button1.BackColor = System.Drawing.Color.DarkOrange;
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(260, 336);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(709, 61);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(73, 27);
             this.button1.TabIndex = 5;
@@ -106,12 +107,12 @@ namespace ProyectoLFA
             // 
             // button2
             // 
-            this.button2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button2.BackColor = System.Drawing.Color.DarkOrange;
             this.button2.ForeColor = System.Drawing.SystemColors.Control;
-            this.button2.Location = new System.Drawing.Point(337, 336);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Location = new System.Drawing.Point(709, 26);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(62, 27);
+            this.button2.Size = new System.Drawing.Size(73, 27);
             this.button2.TabIndex = 6;
             this.button2.Text = "Salir";
             this.button2.UseVisualStyleBackColor = false;
@@ -119,10 +120,10 @@ namespace ProyectoLFA
             // 
             // TransitionBTN
             // 
-            this.TransitionBTN.BackColor = System.Drawing.SystemColors.Desktop;
+            this.TransitionBTN.BackColor = System.Drawing.Color.DarkOrange;
             this.TransitionBTN.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.TransitionBTN.Location = new System.Drawing.Point(183, 330);
-            this.TransitionBTN.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TransitionBTN.Location = new System.Drawing.Point(709, 97);
+            this.TransitionBTN.Margin = new System.Windows.Forms.Padding(2);
             this.TransitionBTN.Name = "TransitionBTN";
             this.TransitionBTN.Size = new System.Drawing.Size(73, 38);
             this.TransitionBTN.TabIndex = 7;
@@ -130,12 +131,25 @@ namespace ProyectoLFA
             this.TransitionBTN.UseVisualStyleBackColor = false;
             this.TransitionBTN.Click += new System.EventHandler(this.button3_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(63, 32);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Ingrese la Gramatica";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
             // FileAnalyser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Highlight;
-            this.ClientSize = new System.Drawing.Size(412, 372);
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(840, 236);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.TransitionBTN);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -144,7 +158,9 @@ namespace ProyectoLFA
             this.Controls.Add(this.TResult);
             this.Controls.Add(this.RTBGrammar);
             this.Controls.Add(this.BTNUpload);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "FileAnalyser";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -162,6 +178,7 @@ namespace ProyectoLFA
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button TransitionBTN;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- ProyectoLFA/ProyectoLFA/TransitionsView.Designer.cs
+++ ProyectoLFA/ProyectoLFA/TransitionsView.Designer.cs
@@ -44,103 +44,99 @@ namespace ProyectoLFA
             // 
             // RTXtext
             // 
+            this.RTXtext.BackColor = System.Drawing.Color.Khaki;
             this.RTXtext.Font = new System.Drawing.Font("SimSun-ExtB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.RTXtext.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.RTXtext.Location = new System.Drawing.Point(16, 54);
-            this.RTXtext.Margin = new System.Windows.Forms.Padding(4);
+            this.RTXtext.Location = new System.Drawing.Point(12, 44);
             this.RTXtext.Name = "RTXtext";
-            this.RTXtext.Size = new System.Drawing.Size(436, 116);
+            this.RTXtext.Size = new System.Drawing.Size(328, 95);
             this.RTXtext.TabIndex = 0;
             this.RTXtext.Text = "";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.DarkGoldenrod;
-            this.label1.Location = new System.Drawing.Point(16, 11);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(144, 40);
+            this.label1.Size = new System.Drawing.Size(94, 29);
             this.label1.TabIndex = 1;
             this.label1.Text = "TOKENS";
             // 
             // FollowTable
             // 
+            this.FollowTable.BackgroundColor = System.Drawing.Color.Khaki;
             this.FollowTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.FollowTable.Location = new System.Drawing.Point(24, 225);
-            this.FollowTable.Margin = new System.Windows.Forms.Padding(4);
+            this.FollowTable.Location = new System.Drawing.Point(383, 154);
             this.FollowTable.Name = "FollowTable";
             this.FollowTable.RowHeadersVisible = false;
             this.FollowTable.RowHeadersWidth = 51;
-            this.FollowTable.Size = new System.Drawing.Size(373, 270);
+            this.FollowTable.Size = new System.Drawing.Size(285, 219);
             this.FollowTable.TabIndex = 2;
             // 
             // TreeTable
             // 
+            this.TreeTable.BackgroundColor = System.Drawing.Color.Khaki;
             this.TreeTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.TreeTable.GridColor = System.Drawing.SystemColors.InactiveCaption;
-            this.TreeTable.Location = new System.Drawing.Point(495, 85);
-            this.TreeTable.Margin = new System.Windows.Forms.Padding(4);
+            this.TreeTable.Location = new System.Drawing.Point(12, 385);
             this.TreeTable.Name = "TreeTable";
             this.TreeTable.RowHeadersVisible = false;
             this.TreeTable.RowHeadersWidth = 51;
-            this.TreeTable.Size = new System.Drawing.Size(511, 140);
+            this.TreeTable.Size = new System.Drawing.Size(328, 101);
             this.TreeTable.TabIndex = 3;
             // 
             // TransitionsTable
             // 
+            this.TransitionsTable.BackgroundColor = System.Drawing.Color.Khaki;
             this.TransitionsTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.TransitionsTable.Location = new System.Drawing.Point(495, 327);
-            this.TransitionsTable.Margin = new System.Windows.Forms.Padding(4);
+            this.TransitionsTable.Location = new System.Drawing.Point(12, 198);
             this.TransitionsTable.Name = "TransitionsTable";
             this.TransitionsTable.RowHeadersVisible = false;
             this.TransitionsTable.RowHeadersWidth = 51;
-            this.TransitionsTable.Size = new System.Drawing.Size(511, 167);
+            this.TransitionsTable.Size = new System.Drawing.Size(328, 136);
             this.TransitionsTable.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Tempus Sans ITC", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.SteelBlue;
-            this.label2.Location = new System.Drawing.Point(491, 293);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label2.Location = new System.Drawing.Point(14, 169);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(162, 19);
+            this.label2.Size = new System.Drawing.Size(119, 16);
             this.label2.TabIndex = 5;
             this.label2.Text = "Tabla de transiciones";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Tempus Sans ITC", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.SteelBlue;
-            this.label3.Location = new System.Drawing.Point(491, 54);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label3.Location = new System.Drawing.Point(14, 357);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(245, 22);
+            this.label3.Size = new System.Drawing.Size(162, 16);
             this.label3.TabIndex = 6;
             this.label3.Text = "Tabla de First, Last y Nullable\r\n";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Tempus Sans ITC", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.SteelBlue;
-            this.label4.Location = new System.Drawing.Point(20, 194);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label4.Location = new System.Drawing.Point(473, 123);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(125, 19);
+            this.label4.Size = new System.Drawing.Size(89, 16);
             this.label4.TabIndex = 7;
             this.label4.Text = "Tabla de Follow";
             // 
             // TransitionsView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Info;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(715, 541);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -149,7 +145,6 @@ namespace ProyectoLFA
             this.Controls.Add(this.FollowTable);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.RTXtext);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "TransitionsView";
             this.Text = "TransitionsView";
             this.Load += new System.EventHandler(this.TransitionsView_Load);
