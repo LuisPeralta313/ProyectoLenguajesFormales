b11ac28fd75869dad3bcf5f547bdb6c039680d67
Rodrigo <rodriept429@gmail.com>
2024-05-10 14:08:12 -0600

    implementacion de la clase DFA

---
+++ ProyectoLFA/ProyectoLFA/Clases/DFA.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoLFA.Clases
+{
+    internal class DFA
+    {
+        public FollowTable states;
+
+        public DFA(ET tree)
+        {
+            states = new FollowTable(tree);
+            TransitionT transitionTable = new TransitionT(states);
+        }
+
+        //todo DFA with sets
+        public DFA(ET tree, Dictionary<string, int[]> sets)
+        {
+            states = new FollowTable(tree);
+            TransitionT transitionTable = new TransitionT(states);
+        }
+
+        public bool isValidString(string text, ref string message, ref int countCharacters)
+        {
+            //Evaluate character in table position
+            //Go through all possible next positions and find a match
+            //count number of times that was validated until error
+            //if any of the possible follows is match return suggested char of the one with the longest chain
+
+            bool result = false;
+            Stack<char> characters = new Stack<char>();
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                characters.Push(text[i]);
+            }
+
+            message = traverseTable(characters, ref result, ref countCharacters);
+
+            return result;
+        }
+
+        private string traverseTable(Stack<char> expression, ref bool isCorrect, ref int characters)
+        {
+            string expectedValue = "";
+            int logestPath = 0;
+            bool isCorrectString = false;
+
+            int count = pathCount(expression, 0, ref expectedValue, ref logestPath, ref isCorrectString);
+
+            characters = count;
+
+            if (!isCorrectString)
+            {
+                if (expectedValue != "")
+                {
+                    expectedValue = "Se esperaba " + expectedValue;
+                }
+
+                isCorrect = false;
+                return $"Error de formato: {expectedValue}";
+            }
+
+            isCorrect = true;
+            return "Formato Correcto";
+        }
+
+        private int pathCount(Stack<char> expression, int position, ref string expectedValue,
+            ref int longestPath, ref bool isCorrect)
+
+        {
+            string thisNodeFollowValues = "";
+
+            //If next is end node
+            if (expression.Count == 0)
+            {
+                foreach (var item in states.nodes[position].follows)
+                {
+                    //Save possible next values if any of these appear 
+                    if (thisNodeFollowValues != "")
+                    {
+                        thisNodeFollowValues = $"{thisNodeFollowValues} | {states.nodes[item].character}";
+                    }
+                    else
+                    {
+                        thisNodeFollowValues = $"{states.nodes[item].character}";
+                    }
+
+                    if (states.nodes[item].isAcceptanceStatus)
+                    {
+                        isCorrect = true;
+                        return 1;
+                    }
+                }
+
+                //Do not show # as an expected value
+                expectedValue = states.nodes[position].isAcceptanceStatus ?
+                                "menos caracteres" : thisNodeFollowValues;
+
+                return 0;
+            }
+            //If end node and there still items in stack
+            else if (states.nodes[position].isAcceptanceStatus)
+            {
+                return 5;
+            }
+            //if middle node
+            else
+            {
+                //Character of this iteration
+                char nextCharacter = expression.Pop();
+                string tmpExpectedValue = "";
+                int tmpLongestPath = 0;
+                bool foundNextValue = false;
+                bool jumpFound = nextCharacter == '\n'; //Ya no se que hacer :(
+
+                if (jumpFound)
+                {
+                    nextCharacter = ' ';
+                }
+
+                //Traverse follow values
+                foreach (var item in states.nodes[position].follows)
+                {
+                    //Save possible next values if any of these appear 
+                    if (thisNodeFollowValues != "")
+                    {
+                        thisNodeFollowValues = $"{thisNodeFollowValues} | {states.nodes[item].character}";
+                    }
+                    else
+                    {
+                        thisNodeFollowValues = $"{states.nodes[item].character}";
+                    }
+
+                    if (states.nodes[item].character == nextCharacter.ToString() || states.nodes[item].isAcceptanceStatus ||
+                        (states.nodes[item].character == CharSET.MayusChar && char.IsUpper(nextCharacter))
+                        || (states.nodes[item].character == CharSET.MinusChar && char.IsLower(nextCharacter))
+                        || (states.nodes[item].character == CharSET.Numbers && char.IsDigit(nextCharacter))
+                        || (states.nodes[item].character == CharSET.Symbols && (char.IsSymbol(nextCharacter) || char.IsPunctuation(nextCharacter))))
+                    {
+                        //Mark flag for later use
+                        foundNextValue = true;
+
+                        //Check if this is not the correct path
+                        int tmp = pathCount(expression, item, ref tmpExpectedValue,
+                            ref tmpLongestPath, ref isCorrect);
+
+                        if (tmp == 5) //If final status is reached but there were still char in stack
+                        {
+                            tmpExpectedValue += states.nodes[position].character;
+                        }
+
+                        if (tmp >= longestPath)
+                        {
+                            expectedValue = tmpExpectedValue;
+                            longestPath = tmp;
+                        }
+
+                    }
+
+                    if (isCorrect || (jumpFound && foundNextValue))
+                    {
+                        break;
+                    }
+                }
+
+                //Analyze results for recursion
+                if (isCorrect)
+                {
+                    return 0;
+                }
+                else if (foundNextValue)
+                {
+                    expression.Push(nextCharacter); //Return item for next iteration
+                    return longestPath + 1;
+                }
+                else
+                {
+                    expression.Push(nextCharacter); //Return item for next iteration
+                    expectedValue = thisNodeFollowValues;
+                    return 1;
+                }
+            }
+        }
+    }
+}
