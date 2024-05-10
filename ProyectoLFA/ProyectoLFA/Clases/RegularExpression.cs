cd79b8048dba156bfe9cb3304d3dd34cec827c01
WalterVillatoro1204 <wevillatorom@correo.url.edu.gt>
2024-05-10 11:09:26 -0600

    Verificar si la gramatica esta en e formato correcto

--- ProyectoLFA/ProyectoLFA/Clases/RegularExpression.cs
+++ ProyectoLFA/ProyectoLFA/Clases/RegularExpression.cs
@@ -18,7 +18,29 @@ namespace ProyectoLFA.Clases
         // Método para simplificar la expresión regular (actualmente no hace nada)
         private string simplifyExpression(string expression)
         {
+            expression = expression.Replace(AbrevLetrasMayus, MayusChar);
+            expression = expression.Replace(AbrevLetrasMinus, MinusChar);
+            expression = expression.Replace(AbrevNumbers, Numbers);
+            expression = expression.Replace(AbrevSymbols, Symbols);
+
             return expression;
+        }
+
+        public string ValidateString(string text)
+        {
+            //It verifies if the grammar has the correct format
+
+
+            string message = "";
+
+            //bool isValid = AFD.isValidString(text, ref message, ref characters);
+
+            message = message.Replace(MayusChar, AbrevLetrasMayus);
+            message = message.Replace(MinusChar, AbrevLetrasMinus);
+            message = message.Replace(Numbers, AbrevNumbers);
+            message = message.Replace(Symbols, AbrevSymbols);
+
+            return message;
         }
     }
 }
